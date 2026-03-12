56efeb4626385213caab082b4a5459f5b6342c8b
Prasun Dutta <prasundutta87@gmail.com>
2026-03-12 17:52:43 +0000

    updated get_gene_specific_variants_AggV3.sh

--- bin/.ipynb_checkpoints/get_gene_specific_variants_AggV3-checkpoint.sh
+++ bin/.ipynb_checkpoints/get_gene_specific_variants_AggV3-checkpoint.sh
@@ -25,20 +25,20 @@ printf "variant_id\t$(bcftools +split-vep -l "$first_vcf_line" -l | cut -f 2 | p
 
 #Loop through multiple lines if a longer gene is present across two VCF chunks
 while read line;do
-	variant_vcf="${line/s3:\/\//\/mnt\/file-systems\/}"
+	variant_vcf="$line"
 	bcftools view -Ou -r "$chrom":"$region_start"-"$end" -f 'PASS,.' --threads 4 "$variant_vcf" |  \
 	bcftools filter -i 'AF <= 0.01' --threads 4 -Ou | \
 	bcftools query -i 'GT="alt"' -f '[%SAMPLE\t%CHROM\_%POS\_%REF\_%ALT\t%FILTER\t%GT\t%AC\t%AN\t%AF\n]' >>"$gene"_genotypes.tsv
 done < "$genotype_vcfs"
 
 while read line;do
-	siteQC_vcf="${line/s3:\/\//\/mnt\/file-systems\/}"
+	siteQC_vcf="$line"
 	bcftools view -Ou -r "$chrom":"$region_start"-"$end" --threads 4 "$siteQC_vcf" |  \
 	bcftools query -f '%CHROM\_%POS\_%REF\_%ALT\t%AC\t%AN\t%AF\t%AC_Hom\t%AC_Het\t%AC_Hemi\t%MEDIAN_DP\t%MEDIAN_GQ\t%AB_RATIO\t%MISSINGNESS_RATE\n' >>"$gene"_siteQC.tsv
 done < "$siteqc_vcfs"
 
 while read line;do
-    annotation_vcf="${line/s3:\/\//\/mnt\/file-systems\/}"
+    annotation_vcf="$line"
 	bcftools +split-vep -r "$chrom":"$region_start"-"$end" "$annotation_vcf" -d \
     -i "SYMBOL == \"$gene\" && (MANE_SELECT != \".\" || CANONICAL == \"YES\")" \
 	-a CSQ -A tab \
