ac6cf7dea483d1b17db8c7f540732c5a5fddb566
Prasun Dutta <prasundutta87@gmail.com>
2026-03-12 14:49:59 +0000

    updated find_gene_shards.sh

--- bin/find_gene_shards.sh
+++ bin/find_gene_shards.sh
@@ -2,7 +2,7 @@
 
 ROW_INDEX="$1"
 GENES_BED="$2"
-BIALLELIC_SHARDS="$3"
+BIALLELIC_GENOTYPE_SHARDS="$3"
 ANNO_SHARDS="$4"
 SITEQC_SHARDS="$5"
 
@@ -12,7 +12,7 @@ echo "$LINE" > temp_${ROW_INDEX}.bed
 gene=$(cut -f4 temp_${ROW_INDEX}.bed)
 
 ##get genotype information
-bedtools intersect -wo -a temp_${ROW_INDEX}.bed -b "${BIALLELIC_SHARDS}" > VCF_genotypes_${ROW_INDEX}.bed
+bedtools intersect -wo -a temp_${ROW_INDEX}.bed -b "${BIALLELIC_GENOTYPE_SHARDS}" > VCF_genotypes_${ROW_INDEX}.bed
 
 ##get functional annotation information
 bedtools intersect -wo -a temp_${ROW_INDEX}.bed -b "${ANNO_SHARDS}" > VCF_annotations_${ROW_INDEX}.bed
@@ -21,7 +21,7 @@ bedtools intersect -wo -a temp_${ROW_INDEX}.bed -b "${ANNO_SHARDS}" > VCF_annota
 bedtools intersect -wo -a temp_${ROW_INDEX}.bed -b "${SITEQC_SHARDS}" > VCF_siteQC_${ROW_INDEX}.bed
 
 # Extract only the VCF paths from column-13
-cat VCF_genotypes_${ROW_INDEX}.bed | cut -f13 > ${gene}_biallelic_shards.txt
+cat VCF_genotypes_${ROW_INDEX}.bed | cut -f13 > ${gene}_biallelic_genotype_shards.txt
 cat VCF_annotations_${ROW_INDEX}.bed | cut -f13 > ${gene}_annotation_shards.txt
 cat VCF_siteQC_${ROW_INDEX}.bed |cut -f13 > ${gene}_siteqc_shards.txt
 
